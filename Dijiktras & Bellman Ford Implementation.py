4a6bceac1b37476c3db7556a95b94665c60ad477
Sai Rishyanth Visinigiri <66677354+vRishy@users.noreply.github.com>
2023-05-03 11:56:53 +0530

    Add files via upload

---
+++ Dijiktras & Bellman Ford Implementation.py
@@ -0,0 +1,144 @@
+# Python program for Dijkstra's Algorithm
+# Library for INT_MAX
+import sys
+
+class Graph():
+
+	def __init__(self, vertices):
+		self.V = vertices
+		self.graph = [[0 for column in range(vertices)]
+					for row in range(vertices)]
+
+	def printSolution(self, dist):
+		print("Vertex \tDistance from Source")
+		for node in range(self.V):
+			print(node, "\t", dist[node])
+
+	# A utility function to find the vertex with
+	# minimum distance value, from the set of vertices
+	# not yet included in shortest path tree
+	def minDistance(self, dist, sptSet):
+
+		# Initialize minimum distance for next node
+		min = sys.maxsize
+
+		# Search not nearest vertex not in the
+		# shortest path tree
+		for u in range(self.V):
+			if dist[u] < min and sptSet[u] == False:
+				min = dist[u]
+				min_index = u
+
+		return min_index
+
+	# Function that implements Dijkstra's single source
+	# shortest path algorithm for a graph represented
+	# using adjacency matrix representation
+	def dijkstra(self, src):
+
+		dist = [sys.maxsize] * self.V
+		dist[src] = 0
+		sptSet = [False] * self.V
+
+		for cout in range(self.V):
+
+			# Pick the minimum distance vertex from
+			# the set of vertices not yet processed.
+			# x is always equal to src in first iteration
+			x = self.minDistance(dist, sptSet)
+
+			# Put the minimum distance vertex in the
+			# shortest path tree
+			sptSet[x] = True
+
+			# Update dist value of the adjacent vertices
+			# of the picked vertex only if the current
+			# distance is greater than new distance and
+			# the vertex in not in the shortest path tree
+			for y in range(self.V):
+				if self.graph[x][y] > 0 and sptSet[y] == False and \
+						dist[y] > dist[x] + self.graph[x][y]:
+					dist[y] = dist[x] + self.graph[x][y]
+
+		self.printSolution(dist)
+
+
+# Driver's code
+if __name__ == "__main__":
+	g = Graph(9)
+	g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+			[4, 0, 8, 0, 0, 0, 0, 11, 0],
+			[0, 8, 0, 7, 0, 4, 0, 0, 2],
+			[0, 0, 7, 0, 9, 14, 0, 0, 0],
+			[0, 0, 0, 9, 0, 10, 0, 0, 0],
+			[0, 0, 4, 14, 10, 0, 2, 0, 0],
+			[0, 0, 0, 0, 0, 2, 0, 1, 6],
+			[8, 11, 0, 0, 0, 0, 1, 0, 7],
+			[0, 0, 2, 0, 0, 0, 6, 7, 0]
+			]
+
+	g.dijkstra(0)
+
+
+# Bellman Ford Algorithm in Python
+class Graph:
+
+    def __init__(self, vertices):
+        self.V = vertices   # Total number of vertices in the graph
+        self.graph = []     # Array of edges
+
+    # Add edges
+    def add_edge(self, s, d, w):
+        self.graph.append([s, d, w])
+
+    # Print the solution
+    def print_solution(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    def bellman_ford(self, src):
+
+        # Step 1: fill the distance array and predecessor array
+        dist = [float("Inf")] * self.V
+        # Mark the source vertex
+        dist[src] = 0
+
+        # Step 2: relax edges |V| - 1 times
+        for _ in range(self.V - 1):
+            for s, d, w in self.graph:
+                if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+        # Step 3: detect negative cycle
+        # if value changes then we have a negative cycle in the graph
+        # and we cannot find the shortest distances
+        for s, d, w in self.graph:
+            if dist[s] != float("Inf") and dist[s] + w < dist[d]:
+                print("Graph contains negative weight cycle")
+                return
+
+        # No negative weight cycle found!
+        # Print the distance and predecessor array
+        self.print_solution(dist)
+
+
+g = Graph(9)
+
+g.add_edge(0, 1, 4)
+g.add_edge(0, 7, 8)
+g.add_edge(1, 2, 8)
+g.add_edge(1, 7, 11)
+g.add_edge(2, 3, 7)
+g.add_edge(2, 8, 2)
+g.add_edge(2, 5, 4)
+g.add_edge(3, 4, 9)
+g.add_edge(3, 5, 14)
+g.add_edge(4, 5, 10)
+g.add_edge(5, 6, 2)
+g.add_edge(6, 7, 1)
+g.add_edge(6, 8, 6)
+g.add_edge(7, 8, 7)
+g.bellman_ford(0)
+
+
